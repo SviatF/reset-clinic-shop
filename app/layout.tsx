bd8d7f80dc4d3f2bdb97febc9a90115e15cc8f79
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-09-02 06:25:15 -0700

    Load premium product card styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./conversion.css";
 import "./conversion-extras.css";
 import "./account-conversion.css";
 import "./trust-layer.css";
+import "./premium-product-cards.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import CartDrawer from "@/components/CartDrawer";
