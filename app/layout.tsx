ee6849bf2fc412ee3ea75357421af71135883009
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-09-02 05:23:24 -0700

    Load certificate imagery fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "./refinements.css";
 import "./secondary-pages.css";
+import "./certificate-fixes.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { CartProvider } from "@/components/CartProvider";
