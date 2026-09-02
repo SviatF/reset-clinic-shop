a2c7eadde594560aa44f00364b33db56fc77365c
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-09-02 08:22:25 -0700

    Add admin orders page

---
+++ app/admin/(secure)/orders/page.tsx
@@ -0,0 +1,7 @@
+import AdminOrdersClient from "@/components/AdminOrdersClient";
+
+export const dynamic = "force-dynamic";
+
+export default function AdminOrdersPage() {
+  return <AdminOrdersClient />;
+}
