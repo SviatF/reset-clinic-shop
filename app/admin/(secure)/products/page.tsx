9d54806d078af21bbbf11c3971ecd6cb4bcb1064
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-09-02 08:22:17 -0700

    Add admin products page

---
+++ app/admin/(secure)/products/page.tsx
@@ -0,0 +1,7 @@
+import AdminProductsClient from "@/components/AdminProductsClient";
+
+export const dynamic = "force-dynamic";
+
+export default function AdminProductsPage() {
+  return <AdminProductsClient />;
+}
