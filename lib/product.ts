998da866f355eefe099787ce69ac6e8627147441
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-09-02 07:10:25 -0700

    Enrich product data with official Aesop details

--- lib/product.ts
+++ lib/product.ts
@@ -3,8 +3,20 @@ export const product = {
   name: "Parsley Seed Anti-Oxidant Eye Cream",
   price: 73,
   currency: "₴",
+  displayCurrency: "грн",
+  size: "10 mL",
   description:
     "Антиоксидантний крем для делікатної зони навколо очей. Допомагає підтримувати зволоженість, комфорт і доглянутий вигляд шкіри.",
+  suitedTo: "Широкий спектр типів шкіри, особливо суха",
+  skinFeel: "Живлення та зволоження з гладким фінішем",
+  keyIngredients: ["Вітамін C", "Вітамін E", "Sodium Lactate"],
+  howToUse:
+    "Нанесіть невелику кількість на очищену шкіру навколо очей легкими поплескувальними рухами та м’яко вмасажуйте до поглинання.",
+  dosage: "Кількість на кінчику пальця",
+  texture: "Насичений крем",
+  aroma: "Трав’яний, квітковий, свіжий",
+  inci:
+    "Water (Aqua), Glyceryl Stearate SE, Butyrospermum Parkii (Shea) Butter, Stearyl Alcohol, Glycerin, Simmondsia Chinensis (Jojoba) Seed Oil, Theobroma Cacao (Cocoa) Seed Butter, Cetearyl Alcohol, Sodium Ascorbyl Phosphate, Candelilla/Jojoba/Rice Bran Polyglyceryl-3 Esters, Glyceryl Stearate, Tocopherol, Lavandula Angustifolia (Lavender) Oil, 1,2-Hexanediol, Benzyl Alcohol, Sodium Stearoyl Lactylate, Glycine Soja (Soybean) Oil, Copernicia Cerifera (Carnauba) Wax, Microcrystalline Cellulose, Sodium Lactate, Xanthan Gum, Sodium Stearoyl Glutamate, Citric Acid, Sodium Dehydroacetate, Cellulose Gum, Ormenis Multicaulis Oil, Sodium Gluconate, Camellia Sinensis Leaf Extract, Carum Petroselinum (Parsley) Seed Oil, Pelvetia Canaliculata Extract, Sodium Citrate, Dehydroacetic Acid, Linalool, Limonene, Geraniol.",
 };
 
 export const productHref = `/product/${product.slug}`;
