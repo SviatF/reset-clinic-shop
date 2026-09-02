d23144beb4eafc6c3ee238c87252bc54f95c8180
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-09-02 06:34:29 -0700

    Load ultra premium art direction last

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import "./account-conversion.css";
 import "./trust-layer.css";
 import "./premium-product-cards.css";
 import "./premium-product-cards-compat.css";
+import "./ultra-premium.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import CartDrawer from "@/components/CartDrawer";
